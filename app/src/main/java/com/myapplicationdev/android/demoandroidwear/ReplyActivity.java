fb91b545c52f2df4a063fea7d5f6c812dfc3458a
Gerad20 <57203289+Gerad20@users.noreply.github.com>
2020-08-14 14:44:26 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoandroidwear/ReplyActivity.java
@@ -0,0 +1,30 @@
+package com.myapplicationdev.android.demoandroidwear;
+
+import android.app.RemoteInput;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReplyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.replyactivity);
+
+        CharSequence reply = null;
+        Intent intent = getIntent();
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null){
+            reply = remoteInput.getCharSequence("status");
+        }
+
+        if(reply != null){
+            Toast.makeText(ReplyActivity.this, "You have indicated: " + reply,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
